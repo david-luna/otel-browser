8ff6ec93fc4238728f4ea065dbe19a02e5f9b5f5
David Luna <luna.bistuer@gmail.com>
2025-10-22 23:38:36 +0200

    chore: refactor RN app

---
+++ examples/react-native/app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import List from '@/components/list';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <List></List>
+    </View>
+  );
+}
