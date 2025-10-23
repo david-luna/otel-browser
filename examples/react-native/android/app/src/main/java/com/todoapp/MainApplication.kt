b152fc829782a246cd8c14fa1e9785d2e6137883
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-10-23 17:58:26 +0200

    Adding edot android

--- examples/react-native/android/app/src/main/java/com/todoapp/MainApplication.kt
+++ examples/react-native/android/app/src/main/java/com/todoapp/MainApplication.kt
@@ -1,6 +1,10 @@
 package com.todoapp
 
 import android.app.Application
+import co.elastic.otel.android.ElasticApmAgent
+import co.elastic.otel.android.api.ElasticOtelAgent
+import co.elastic.otel.android.extensions.log
+import co.elastic.otel.android.extensions.span
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -8,6 +12,9 @@ import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
 class MainApplication : Application(), ReactApplication {
+  companion object {
+      internal lateinit var agent: ElasticOtelAgent
+  }
 
   override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
@@ -22,6 +29,17 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    // Initializing EDOT Android
+    agent = ElasticApmAgent.builder(this)
+      .setServiceName("My app name")
+      .setExportUrl("http://10.0.2.2:4318")
+      .build()
+
+    // Creating data for testing purposes
+    agent.span("Creating android app") {
+      agent.log("Log during android app creation")
+    }
+
     loadReactNative(this)
   }
 }
